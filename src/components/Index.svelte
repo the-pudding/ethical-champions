4ae4443ac0b4bfcd62af0c7884c48cabab61ccbc
Russell Samora <russellsamora@gmail.com>
2026-06-01 13:45:28 -0400

    first

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,11 +1,29 @@
 <script>
 	import { getContext } from "svelte";
+	import CMS from "$components/helpers/CMS.svelte";
 	import Footer from "$components/Footer.svelte";
+	import Placeholder from "$components/Placeholder.svelte";
 
-	// const copy = getContext("copy");
-	// const data = getContext("data");
+	const copy = getContext("copy");
+	const { body } = copy;
+	const components = {
+		Placeholder
+	};
 </script>
+
+<article>
+	<CMS {body} {components} />
+</article>
 
 <svelte:boundary onerror={(e) => console.error(e)}>
 	<!-- <Footer recirc={true} /> -->
 </svelte:boundary>
+
+<style>
+	article > :global(section p),
+	article > :global(section video),
+	article > :global(section div) {
+		max-width: var(--col-width);
+		margin: 2rem auto;
+	}
+</style>
---
+++ src/components/Placeholder.svelte
@@ -0,0 +1,18 @@
+<script>
+	let { value } = $props();
+</script>
+
+<div>
+	<p>{value}</p>
+</div>
+
+<style>
+	div {
+		width: 20rem;
+		height: 20rem;
+		background: #eee;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
